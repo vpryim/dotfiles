1d2c73b259352a3d339221fb68a7ab26e39e67e0
Vasyl Pryimachuk <vpryim@gmail.com>
2024-06-21 21:13:16 +0300

    Keymap for splits

--- .config/nvim/lua/vpryim/keymap.lua
+++ .config/nvim/lua/vpryim/keymap.lua
@@ -4,8 +4,6 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Page down' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Page up' })
 vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Find next' })
 vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Find prev' })
-vim.keymap.set('n', '<leader>\\', ':vsplit<Return><C-w>l', { desc = 'Split window' })
-vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>l', { desc = 'Split vertically' })
 
 -- persistence
 vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end,
@@ -14,3 +12,12 @@ vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last 
   { desc = "Restore the last session" })
 vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end,
   { desc = "Stop persistence => session won't be saved on exit" })
+
+-- split
+vim.keymap.set('n', '<leader>\\', ':vsplit<Return><C-w>l', { desc = 'Split window' })
+vim.keymap.set('n', 'ss', ':split<CR><C-w>j')
+vim.keymap.set('n', 'sv', ':vsplit<CR><C-w>l')
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sl', '<C-w>l')
