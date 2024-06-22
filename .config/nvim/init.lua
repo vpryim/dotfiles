cea526398dcbb127837d33b0fe01a968de11b516
Vasyl Pryimachuk <vpryim@gmail.com>
2024-06-22 21:27:34 +0300

    Add nvim-surround

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -90,6 +90,15 @@ require('lazy').setup({
     config = function()
       require('vpryim.harpoon')
     end
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end
   }
 })
 
